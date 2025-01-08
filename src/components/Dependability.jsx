65880b0240d3ec3d13e05a7c557d0d25e425c2fa
Jaycode01 <josephlamidi01@gmail.com>
2025-01-08 07:18:29 +0100

    Updating the left alignment of the text in the Dependability component

--- src/components/Dependability.jsx
+++ src/components/Dependability.jsx
@@ -16,7 +16,7 @@ function Dependability() {
       <div className="flex md:flex-row w-full md:mt-24 mt-12 md:gap-10 gap-6 flex-col">
         <div className="">
           <img src={OnlineBanking} alt="" className="mx-auto" />
-          <h2 className="text-2xl">Online Banking</h2>
+          <h2 className="text-2xl text-center">Online Banking</h2>
           <br />
           <p className="">
             Our mobile web and mobile application allows you to keep track of
@@ -25,7 +25,7 @@ function Dependability() {
         </div>
         <div className="">
           <img src={SimpleBudgeting} alt="" className="mx-auto" />
-          <h2 className="text-2xl">Simple Budgeting</h2>
+          <h2 className="text-2xl text-center">Simple Budgeting</h2>
           <br />
           <p className="">
             See exactly where your money goes each month. Receive notifications
@@ -34,7 +34,7 @@ function Dependability() {
         </div>
         <div className="">
           <img src={FastOnboarding} alt="" className="mx-auto" />
-          <h2 className="text-2xl">Fast Onboarding</h2>
+          <h2 className="text-2xl text-center">Fast Onboarding</h2>
           <br />
           <p className="">
             We don't do branches. Open your account online and start taking
@@ -43,7 +43,7 @@ function Dependability() {
         </div>
         <div className="">
           <img src={OpenAPI} alt="" className="mx-auto" />
-          <h2 className="text-2xl">Open API</h2>
+          <h2 className="text-2xl text-center">Open API</h2>
           <br />
           <p className="">
             Manage your savings, investments, pension, and much more from one
